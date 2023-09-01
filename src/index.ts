365c2948c2a43dd2b72fcde81442d78ec54dfa7a
yaKsirhC <chrisuser199@gmail.com>
2023-09-01 14:59:29 +0300

    fix duplicate

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ mongoose.connect(process.env.MONGO_URI as string).finally(() => console.log("Mon
 
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: false}));
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(cors({origin: "http://localhost:5173" , credentials: true}))
 app.use(fileUpload())
